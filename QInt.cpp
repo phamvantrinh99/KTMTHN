1a287a9852c1a6f0999a4c6a2ef2aeb1372b6c38
toanmadrid <37981186+toanmadrid@users.noreply.github.com>
2019-03-24 15:14:31 +0700

    Update QInt.cpp

--- QInt.cpp
+++ QInt.cpp
@@ -505,7 +505,7 @@ string BinToHex(bool *bit)
 //Hàm chuyển đổi số QInt thập phân sang nhị phân
 bool* DecToBin(QInt x)
 {
-	bool* res=new bool[128];
+	bool* res = new bool[128];
 	for (size_t i = 0; i < 128; i++)
 	{
 		res[i] = (x.getData()[3 - (127 - i) / 32] & (1 << ((127 - i) % 32))) >> ((127 - i) % 32);
@@ -554,7 +554,7 @@ string StrDivTwo(const string &Str)
 	return Result;
 }
 
-//Chuyển chuỗi số nguyên string x sang chuỗi nhị phân kiểu bool
+//Chuyển chuỗi số nguyên he 10 string x sang chuỗi nhị phân kiểu bool
 bool* CharToBit(string x)
 {
 	bool Negative = false;
@@ -580,4 +580,245 @@ bool* CharToBit(string x)
 		Result = DecToBin(Result2); //Đưa lại về nhị phân.
 	}
 	return Result;
+}
+
+
+//Hàm tính a lũy thừa n (a, n là số nguyên không âm).
+string Power(int a, int n)
+{
+	string Result;
+
+	if (a > -1)
+	{
+		//Nếu a > 1
+		if (a > 1)
+		{
+			int Pow = n;
+
+			if (n > 31 * 2 / a) //Tránh trường hợp tràn số.
+			{
+				Pow = 31 * 2 / a;
+			}
+
+			Result = to_string(unsigned int(pow(a, Pow)));
+
+
+			//Phần có khả năng bị tràn số sẽ được nhân theo nhân chuỗi.
+			for (int i = Pow + 1; i <= n; i++)
+			{
+				Result = MultiplyNumberString(Result, to_string(a));
+			}
+		}
+		else
+		{
+			Result.push_back(a + '0');
+		}
+	}
+
+	return Result;
+}
+
+//Hàm nhân hai chuỗi.
+string MultiplyNumberString(string a, string b)
+{
+	string Result = "0";
+	string TempStr;
+	int Temp = 0;
+	bool Negative = false;
+
+	if (a[0] == '-')
+	{
+		if (b[0] != '-')
+		{
+			Negative = true;
+		}
+		a.erase(a.begin());
+	}
+	if (b[0] == '-')
+	{
+		if (a[0] != '-')
+		{
+			Negative = true;
+		}
+		b.erase(b.begin());
+	}
+
+	for (int i = 0; i < b.size(); i++)
+	{
+		for (int j = a.size() - 1; j > -1; j--)
+		{
+			Temp = Temp + (a[j] - '0') * (b[b.size() - i - 1] - '0');
+			TempStr.push_back((Temp % 10) + '0');
+			Temp = Temp / 10;
+		}
+		if (Temp != 0) //Nếu vẫn còn Temp.
+		{
+			TempStr.push_back(Temp + '0');
+			Temp = 0;
+		}
+
+		//Đảo chuỗi.
+		reverse(TempStr.begin(), TempStr.end());
+
+		//Thêm các số 0 cần thiết vào sau.
+		for (int j = 0; j < i; j++)
+		{
+			TempStr.push_back('0');
+		}
+
+		//Cộng Result và Temp.
+		Result = AddNumberString(Result, TempStr);
+		TempStr.clear(); //Dọn chuỗi tạm để dùng cho các bước tiếp theo.
+	}
+
+	if (Negative == true)
+	{
+		Result.insert(Result.begin(), '-');
+	}
+
+	return Result;
+}
+
+//Hàm cộng hai chuỗi số.
+string AddNumberString(string a, string b)
+{
+	if (a[0] == '-' && b[0] != '-')
+	{
+		a.erase(a.begin());
+		a = SubNumberString(b, a);
+	}
+	else
+	{
+		if (b[0] == '-' && a[0] != '-')
+		{
+			b.erase(b.begin());
+			a = SubNumberString(a, b);
+		}
+		else
+		{
+			int Temp = 0;
+			bool Negative = false;
+
+			if (a[0] == '-' && b[0] == '-')
+			{
+				Negative = true;
+			}
+
+			//Thêm các số 0 vào đầu mỗi chuỗi.
+			if (a.size() > b.size())
+			{
+				while (a.size() != b.size())
+				{
+					b.insert(b.begin(), '0');
+				}
+			}
+			else
+			{
+				while (a.size() != b.size())
+				{
+					a.insert(a.begin(), '0');
+				}
+			}
+
+			//Tiến hành cộng.
+			for (int i = a.size() - 1; i > -1; i--)
+			{
+				Temp = Temp + a[i] + b[i] - 2 * '0';
+				a[i] = (Temp % 10) + '0';
+				Temp = Temp / 10;
+			}
+
+			//Nếu vẫn còn dư (Temp != 0)
+			if (Temp != 0)
+			{
+				a.insert(a.begin(), Temp + '0');
+				Temp = 0;
+			}
+		}
+	}
+
+	//Xóa các số 0 thừa.
+	while (a[0] == '0')
+	{
+		a.erase(a.begin());
+	}
+
+	return a;
+}
+
+//Hàm trừ hai chuỗi số dương.
+string SubNumberString(string a, string b)
+{
+	int Temp = 0;
+	bool Negative = false;
+
+	//Thêm các số 0 vào đầu mỗi chuỗi.
+	if (a.size() > b.size())
+	{
+		while (a.size() != b.size())
+		{
+			b.insert(b.begin(), '0');
+		}
+	}
+	else
+	{
+		while (a.size() != b.size())
+		{
+			a.insert(a.begin(), '0');
+		}
+	}
+
+	//Kiểm tra a hay b lớn hơn và chuyển a thành số lớn hơn.
+	if (strcmp(a.c_str(), b.c_str()) < 0)
+	{
+		string TempStr = a;
+		a = b;
+		b = TempStr;
+		Negative = true; //a < b => a - b < 0
+	}
+
+	//Thực hiện phép trừ.
+	for (int i = a.size() - 1; i > -1; i--)
+	{
+		if ((a[i] - Temp) < b[i])
+		{
+			a[i] = (10 + a[i] - b[i] - Temp) + '0';
+			Temp = 1;
+		}
+		else
+		{
+			a[i] = a[i] - b[i] - Temp + '0';
+			Temp = 0;
+		}
+	}
+
+	//Xóa các số 0 thừa.
+	while (a[0] == '0')
+	{
+		a.erase(a.begin());
+	}
+
+	//Nếu vẫn còn dư (Temp != 0)
+	if (Negative == true)
+	{
+		a.insert(a.begin(), '-');
+	}
+
+	return a;
+}
+
+
+string QInttoDecString(const QInt &x)
+{
+	bool* Binary = DecToBin(x);
+	string Decimal = "0";
+	if (Binary[0] == 1) Decimal = SubNumberString(Decimal, Power(2, 127));
+	for (int i = 1; i < 128; i++)
+	{
+		if (Binary[i] == 1)
+		{
+			Decimal = AddNumberString(Decimal, Power(2, 127 - i));
+		}
+	}
+	return Decimal;
 }
