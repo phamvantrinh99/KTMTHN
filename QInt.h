b1d8188c560d6222c5be6954d99b4e7d826dcd5a
toanmadrid <37981186+toanmadrid@users.noreply.github.com>
2019-03-25 18:00:10 +0700

    Update QInt.h

--- QInt.h
+++ QInt.h
@@ -24,13 +24,17 @@ public:
 
 	//---------------------------------NHÓM TOÁN TỬ VÀ PHÉP TOÁN SỐ HỌC---------------------------------
 	// toan tu *
-	QInt QInt::operator * (QInt x) const;
+	QInt operator * (QInt x) const;
 	// toan tu /
 	QInt QInt::operator / (QInt x);
 	//Toán tử +
-	QInt QInt::operator + (QInt x) const;
+	QInt operator + (QInt x) const;
 	//toan tu -.
-	QInt QInt::operator - (QInt x) const;
+	QInt operator - (QInt x) const;
+	//Toán tử ==
+	bool operator == (QInt x) const;
+	//Toán tử !=
+	bool operator != (QInt x) const;
 	//Toán tử gán =.
 	QInt& operator = (const QInt&x);
 
@@ -62,6 +66,12 @@ public:
 	bool isNegative() const;
 };
 
+//Hàm Nhập
+void ScanQInt(QInt &x);
+
+//Hàm xuất 
+void PrintQInt(QInt x);
+
 //Hàm chuyển đổi số QInt nhị phân sang thập lục phân
 string BinToHex(bool *bit);
 
@@ -75,7 +85,7 @@ string DecToHex(QInt x);
 string StrDivTwo(const string &Str);
 
 //Chuyển chuỗi số nguyên string x sang chuỗi nhị phân kiểu bool
-bool* CharToBit(string x);
+bool* StrQIntDecToBin(string x);
 
 //Hàm tính a lũy thừa n (a, n là số nguyên không âm).
 string Power(int a, int n);
