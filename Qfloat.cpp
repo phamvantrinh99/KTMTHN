6db210352ee342018c4afd79b22c64d6b114ebb9
toanmadrid <37981186+toanmadrid@users.noreply.github.com>
2019-03-26 10:49:26 +0700

    Update Qfloat.cpp

--- Qfloat.cpp
+++ Qfloat.cpp
@@ -1,4 +1,5 @@
 #include "QFloat.h"
+#include "QInt.h"
 
 //Khởi tạo mặc định
 QFloat::QFloat()
@@ -135,168 +136,112 @@ unsigned long long int NguyenSangSo(string x)
 	return num1;
 }
 
+//Nhân số dạng string với 2
+string StrMultiTwo(string Str)
+{
+	string tempres;
+	int temp = 0;
+	bool del = true;
+	for (int i = Str.size() - 1; i >= 0; i--)
+	{
+		if (Str[i] == '.')
+		{
+			tempres.push_back('.');
+			tempres.push_back(temp + '0');
+		}
+		else
+		{
+			temp += 2 * (Str[i] - '0');
+			if (del)
+			{
+				if (temp % 10 != 0)
+				{
+					tempres.push_back((temp % 10) + '0');
+					del = false;
+				}
+				temp /= 10;
+			}
+			else
+			{
+				tempres.push_back((temp % 10) + '0');
+				temp /= 10;
+			}
+		}
+	}
+	string res;
+	for (int i = tempres.size() - 1; i >= 0; i--)
+		res.push_back(tempres[i]);
+	return res;
+}
+
+//Chuyển dãy số thập phân dạng 0.xxx qua hệ 2
+bool* StrFracToBin(string frac)
+{
+	bool* res = new bool[128];
+	memset(res, 0, 128);
+	int i = 0;
+	while (!(frac == "1." || i == 128))
+	{
+		frac.erase(frac.begin());
+		frac = StrMultiTwo(frac);
+		res[i] = (frac[0] - '0');
+		i++;
+	}
+	return res;
+}
+
 //Chuyển số thực Dec dạng string qua bool* nhị phân theo dạng   ±1.F * 2^E
 bool* StrDecToBin(string Dec)
 {
-	bool res[128];
-	string tempDec = Dec;
-	bool Negative = false;
-	if (tempDec[0] == '-') //kiem tra so am
+	bool negative = false;
+	if (Dec[0] == '-')
 	{
-		tempDec.erase(tempDec.begin());
-		Negative = true;
-		res[127] = 1;
+		negative = true;
+		Dec.erase(Dec.begin());
 	}
-	else res[127] = 0;
-
-
-	//tach phan nguyen va frac khoi chuoi
-	bool resFrac[112];
-	string tempRes15Bit;
-	string tempFrac;
-	int size = 0;
-	bool ktCoPhanFrac = false;
-	for (int i = 0; i < Dec.length(); i++)
+	bool* res = new bool[128];
+	memset(res, 0, 128);
+	string nguyen;
+	string frac;
+	while (Dec[0] != '.' && Dec[0] != '\0')
 	{
-		if (Dec[i] == '.')
+		nguyen.push_back(Dec[0]);
+		Dec.erase(Dec.begin());
+	}
+	frac = "0" + Dec;
+	bool* nguyenbin = StrQIntDecToBin(nguyen);
+	bool* fracbin = StrFracToBin(frac);
+	int nguyensize = 0;
+	int E = 16383;
+	int dichfrac = 0;
+	if (nguyen == "0")
+	{
+		dichfrac = 1;
+		while (fracbin[dichfrac - 1] == 0)
+			dichfrac++;
+		E -= dichfrac;
+	}
+	else{
+		for (int i = 127; i >= 0; i--)
 		{
-			ktCoPhanFrac = true;
-			break;
+			if (nguyenbin[i] == 1) nguyensize = 128 - i;
 		}
+		E += nguyensize - 1;
 	}
-	int run = 0;
-	if (ktCoPhanFrac == false)
+	int i = 1;
+	for (; i < 16; i++)
+		res[i] = ((E & (1 << (15 - i))) >> (15 - i));
+	for (; i < 15 + nguyensize; i++)
 	{
-		while (tempDec[run] != '\0')
-		{
-			tempRes15Bit.push_back(tempDec[run]);
-			run++;
-		}
-		tempFrac.push_back('.');
-		tempFrac.push_back('0');
+		res[i] = nguyenbin[113 - nguyensize + i];
 	}
-	else
+	for (; i < 128; i++)
 	{
-		while (tempDec[run] != '.')
-		{
-			tempRes15Bit.push_back(tempDec[run]);
-			run++;
-		}
-		while (tempDec[run] != '\0')
-		{
-			tempFrac.push_back(tempDec[run]);
-			run++;
-		}
+		if (dichfrac != 0) dichfrac--;
+		res[i] = fracbin[dichfrac + i - nguyensize - 15];
 	}
-
-	if (tempFrac[0] == '.') //kiem tra coi co dau cham dau chuoi khong neu co thi xoa
-	{
-		tempFrac.erase(tempFrac.begin());
-	}
-
-	//doi phan so nguyen tu string sang so;
-	unsigned long long int nguyen = NguyenSangSo(tempRes15Bit);
-
-	//doi phan Frac tu string sang so
-	int sizeFrac = tempFrac.size();
-	long double frac = NguyenSangSo(tempFrac) / pow(10, sizeFrac);
-
-	int dichNguyen;
-	DecToBin15Bit(nguyen, dichNguyen, false); //dich qua bao nhieu bit
-	bool* res15Bit = new bool[dichNguyen];
-
-	//doi phan nguyen sang bin
-
-	for (int i = 0; i < dichNguyen; i++)
-	{
-		if (DecToBin15Bit(nguyen, dichNguyen, false)[i] == true)
-		{
-			res15Bit[i] = true;
-		}
-		else res15Bit[i] = false;
-	}
-	dichNguyen--;
-	int soMuE = 16383 + dichNguyen; //so mu E
-
-
-	//chuyen phan da dich (cua nguyen) sang frac
-	int lapDayFrac = 111 - dichNguyen + 1;
-	int tempLDF = lapDayFrac;
-	run = 1;
-	if (lapDayFrac != 111)
-	{
-		for (int i = 111; i >= tempLDF; i--)
-		{
-
-			if (res15Bit[run] == true)
-			{
-				resFrac[i] = true;
-			}
-			else resFrac[i] = false;
-			run++;
-		}
-	}
-
-
-	//doi frac sang bin
-	int dem = 0;
-	while (lapDayFrac != 0)
-	{
-		lapDayFrac--;
-		frac = frac * 2;
-		int fract_bit = frac;
-
-		if (fract_bit == 1)
-		{
-			frac = frac - fract_bit;
-			resFrac[lapDayFrac] = true;
-		}
-		else
-			resFrac[lapDayFrac] = false;
-	}
-
-	//doi so so mu E
-	for (int i = 0; i < 15; i++)
-	{
-		if (DecToBin15Bit(soMuE, dichNguyen, true)[i] == true)
-		{
-			res15Bit[i] = true;
-		}
-		else res15Bit[i] = false;
-	}
-
-	//dua so mu E vao trong chuoi ket qua
-	run = 0;
-	for (int i = 126; i >= 112; i--)
-	{
-		if (res15Bit[run] == true)
-		{
-			res[i] = true;
-		}
-		else res[i] = false;
-		run++;
-
-	}
-	// dua so frac vao ket qua
-	run = 0;
-	for (int i = 0; i < 112; i++)
-	{
-		if (resFrac[run] == true)
-		{
-			res[i] = true;
-		}
-		else res[i] = false;
-		run++;
-	}
-	//doi res lai do code nguoc
-	bool* tempRev = new bool[128];
-	int runRev = 127;
-	for (int i = 0; i < 128; i++)
-	{
-		tempRev[runRev] = res[i];
-		runRev--;
-	}
-	return tempRev;
+	if (negative) res[0] = 1;
+	return res;
 }
 
 //Chuyển đổi số Qfloat nhị phân sang thập phân: 
