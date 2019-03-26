8e237b78f71025c423a5610e217d0846d5e8b9b6
phamvantrinh99 <37787375+phamvantrinh99@users.noreply.github.com>
2019-03-26 13:12:39 +0700

    Update Qfloat.h

--- Qfloat.h
+++ Qfloat.h
@@ -1,8 +1,11 @@
-﻿#ifndef __QFLOAT_H__
+#ifndef __QFLOAT_H__
 #define __QFLOAT_H__
 
 #include <iostream>
 #include <string>
+
+#pragma pack(1)
+
 using namespace std;
 
 class QFloat
@@ -10,25 +13,44 @@ class QFloat
 protected:
 	unsigned int Data[4];
 public:
+	//Khởi tạo mặc định
 	QFloat();
+
+	//Khởi tạo với chuỗi string cơ số 2
 	QFloat(string Binary);
 
+	//Khởi tạo với một mảng bit
+	QFloat(bool* Binary);
+
+	//Hàm lấy dữ liệu
 	unsigned int* getData();
 
+	//Hàm set dữ liệu
 	void setData(int viTrim, unsigned int Data);
 };
 
+//Hàm Nhập
+void ScanQfloat(QFloat &x);
+
+//Hàm xuất
+void PrintQfloat(QFloat x);
+
+//Nhân số dạng string với 2
+string StrMultiTwo(string Str);
+
+//Chuyển dãy số thập phân dạng 0.xxx qua hệ 2
+bool* StrFracToBin(string frac);
+
 //Chuyển số thực Dec dạng string qua bool* nhị phân theo dạng   ±1.F * 2^E
-unsigned long long int NguyenSangSo(string x);
-
-bool* DecToBin15Bit(unsigned long long int Data15Bit, int &size, bool want);
-
 bool* StrDecToBin(string Dec);
 
 //Chuyển đổi số Qfloat nhị phân sang thập phân: 
 QFloat BinToDec(bool *bit);
 
 //Hàm chuyển đổi số Qfloat thập phân sang nhị phân :
-//bool *DecToBin(QFloat x);
+bool *DecToBin(QFloat x);
+
+//Hàm chuyển từ QFloat sang string hệ 10
+string QFloatToDecStr(QFloat x);
 
 #endif
